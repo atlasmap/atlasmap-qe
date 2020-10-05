d49f327ae56ff6cf1cb797997b24a113149d6481
tplevko <tplevko@redhat.com>
2020-10-05 11:21:26 +0200

    fix issue introduced by the new UUID in UI

---
+++ mapping-validator/src/main/java/io/atlasmap/qe/test/MappingDocIdExporter.java
@@ -0,0 +1,44 @@
+package io.atlasmap.qe.test;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MappingDocIdExporter {
+
+    public static String extractDataSourceIdByName(String atlasMappingLocation, String dataSourceName) {
+        String dataSourceId = "";
+        JSONObject atlasMappingJson = exportAtlasJson(atlasMappingLocation);
+        JSONArray dataSourceList = atlasMappingJson.getJSONObject("AtlasMapping").getJSONArray("dataSource");
+
+        for(int i = 0; i < dataSourceList.length(); i++) {
+            if(dataSourceList.getJSONObject(i).getString("name").contentEquals(dataSourceName)) {
+                dataSourceId = dataSourceList.getJSONObject(i).getString("id");
+                break;
+            }
+        }
+        return dataSourceId;
+    }
+
+    private static JSONObject exportAtlasJson(String mappingLocation) {
+        String jsonTxt = "";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream atlasFileStream = classloader.getResourceAsStream(mappingLocation);
+        try {
+            jsonTxt = IOUtils.toString( atlasFileStream );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject atlasJson = new JSONObject(jsonTxt);
+
+        return atlasJson;
+    }
+
+
+}
